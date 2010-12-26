526caa6cc20096c7c2a196fca7e52b4abfdec959
Harmen <harmenstoppels@gmail.com>
2010-12-27 00:13:14 +0100

    Vertaald

--- code/vector.js
+++ code/vector.js
@@ -6,71 +6,71 @@ function Vector(x, y) {
 Vector.prototype.x = Number();
 Vector.prototype.y = Number();
 
-Vector.prototype.add = function(v) {
+Vector.prototype.plus = function(v) {
   this.x += v.x;
   this.y += v.y;
 };
 
-Vector.prototype.getSum = function(v) {
-  return new Vector(this.x + v.x, this.y + v.y);
-};
-
-Vector.prototype.subtract = function(v) {
+Vector.prototype.min = function(v) {
   this.x -= v.x;
   this.y -= v.y;
 };
 
-Vector.prototype.getDifference = function(v) {
+Vector.prototype.keer = function(n) {
+  this.x *= n;
+  this.y *= n;
+};
+
+Vector.prototype.deel = function(n) {
+  this.x /= n;
+  this.y /= n;
+};
+
+Vector.prototype.krijgSom = function(v) {
+  return new Vector(this.x + v.x, this.y + v.y);
+};
+
+Vector.prototype.krijgVerschil = function(v) {
   return new Vector(this.x - v.x, this.y - v.y);
 };
 
-Vector.prototype.multiply = function(scalar) {
-  this.x *= scalar;
-  this.y *= scalar;
+Vector.prototype.krijgProduct = function(n) {
+  return new Vector(this.x * n, this.y * n);
 };
 
-Vector.prototype.getProduct = function(scalar) {
-  return new Vector(this.x * scalar, this.y * scalar);
+Vector.prototype.krijgDeling = function(n) {
+  return new Vector(this.x / n, this.y / n);
 };
 
-Vector.prototype.divide = function(scalar) {
-  this.x /= scalar;
-  this.y /= scalar;
-};
-
-Vector.prototype.getQuotient = function(scalar) {
-  return new Vector(this.x / scalar, this.y / scalar);
-};
-
-Vector.prototype.getMagnitude = function() {
+Vector.prototype.__defineGetter__('norm', function() {
   return Math.sqrt(this.x * this.x + this.y * this.y);
+});
+
+Vector.prototype.normaliseer = function() {
+  this.deel(this.norm);
 };
 
-Vector.prototype.normalize = function() {
-  this.divide(this.getMagnitude());
+Vector.prototype.krijgEenheidsvector = function() {
+  return this.krijgDeling(this.norm);
 };
 
-Vector.prototype.getUnit = function() {
-  return this.getQuotient(this.getMagnitude());
-};
-
-Vector.prototype.dot = function(v) {
+Vector.prototype.inproduct = function(v) {
   return this.x * v.x + this.y * v.y;
 };
 
-Vector.prototype.cross = function(v) {
+Vector.prototype.uitproduct = function(v) {
   return this.x * v.y - this.y * v.x;
 };
 
-Vector.prototype.clone = function() {
+Vector.prototype.kloon = function() {
   return new Vector(this.x, this.y);
 };
 
-Vector.prototype.negate = function() {
+Vector.prototype.spiegel = function() {
   this.x *= -1;
   this.y *= -1;
 };
 
-Vector.project = function(theta, magnitude) {
-  return new Vector(Math.cos(theta) * magnitude, Math.sin(theta) * magnitude);
+Vector.project = function(theta, norm) {
+  return new Vector(Math.cos(theta) * norm, Math.sin(theta) * norm);
 };
