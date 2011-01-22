3726250c1cdc27354984f289e3cb2856bdf86a84
Harmen <harmenstoppels@gmail.com>
2011-01-22 21:23:32 +0100

    Code mooier gemaakt

--- code/javascript.js
+++ code/javascript.js
@@ -1,7 +1,30 @@
-function Vector(x, y){
-    this.dx = x;
-    this.dy = y;
+/**
+ * De Wereld houdt alles bij elkaar
+ */
+function Wereld(breedte, hoogte, ctx){
+  this.breedte = breedte;
+  this.hoogte = hoogte;
+  this.ctx = ctx;
+  this.voorwerpen = [];
 }
+
+/**
+ * Doe een stap in de tijd voor elk voorwerp
+ */
+Wereld.prototype.stap = function(t){
+  
+  // Verplaats elk voorwerp
+  this.voorwerpen.forEach(function(vw, i){
+    
+  });
+};
+
+/**
+ * Teken elk voorwerp op het huidige tijdstip
+ */
+Wereld.prototype.teken = function(){
+  
+};
 
 Vector.prototype.teken = function(ctx, x, y, kleur){
 	ctx.save();
@@ -11,11 +34,11 @@ Vector.prototype.teken = function(ctx, x, y, kleur){
 	ctx.translate(x, y);
 	ctx.beginPath();
 	ctx.moveTo(0, 0);
-	ctx.lineTo(this.dx, this.dy);
+	ctx.lineTo(this.x, this.y);
 	ctx.stroke();
 	
-	ctx.translate(this.dx, this.dy);
-	ctx.rotate(Math.atan2(this.dy, this.dx)-1.25*Math.PI);
+	ctx.translate(this.x, this.y);
+	ctx.rotate(Math.atan2(this.y, this.x)-1.25*Math.PI);
 	
 	ctx.beginPath();
 	ctx.moveTo(0, 0);
@@ -26,34 +49,23 @@ Vector.prototype.teken = function(ctx, x, y, kleur){
 	ctx.restore();
 };
 
-Vector.prototype.add = function(vec){
-	if(vec instanceof Vector){
-		this.dx += vec.dx;
-		this.dy += vec.dy;
-	} else {
-		for(var i=0; i<vec.length; i++){
-			this.dx += vec[i].dx;
-			this.dy += vec[i].dy;
-		}
-	}
-	
-	return this;
-}
-
 function Cirkel(r, px, py){
-    this.straal = r;
-    this.x = px;
-    this.y = py;
+  this.straal = r;
+  this.positie = new Vector(px, py);
 	this.massa = 1000;
 	
-	this.snelheid = new Vector(0, 0);
+	this.snelheid = new Vector();
 	this.krachten = [];
+  this.resulterend = new Vector();
 }
 
 Cirkel.prototype.voegKrachtToe = function(){
-	for(var i=0; i<arguments.length; i++){
-		this.krachten.push(arguments[i]);
+  var args = Array.prototype.slice.call(arguments);
+  
+	for(var i=0; i<args.length; i++){
+		this.krachten.push(args[i]);
 	}
+  
 	return this;
 };
 
@@ -62,38 +74,50 @@ Cirkel.prototype.teken = function(ctx, krachten){
 	
 	// Teken de cirkel
 	ctx.beginPath();
-	ctx.arc(this.x, this.y, this.straal, 0, 2*Math.PI, true);
+	ctx.arc(this.positie.x, this.positie.y, this.straal, 0, 2*Math.PI, true);
 	ctx.fill();
 	
 	if(krachten && krachten===true){
 		// Teken alle krachten
 		for(var i=0; i<this.krachten.length; i++){
-			this.krachten[i].teken(ctx, this.x, this.y);
+			this.krachten[i].teken(ctx, this.positie.x, this.positie.y);
 		}
 		
 		// Teken de resulterende kracht als er meer dan 1 kracht op de cirkel werkt
 		if(this.krachten.length > 1){
-			(new Vector(0, 0)).add(this.krachten).teken(ctx, this.x, this.y, 'green');
+			(new Vector()).plus(this.krachten).teken(ctx, this.positie.x, this.positie.y, 'green');
 		}
 	}
 	
 	return this;
 };
 
-Cirkel.prototype.beweeg = function(){
+/**
+ * Bij de beweging wordt de nieuwe snelheid berekend
+ * gebruikmakend van de krachten van het moment
+ * F = m*a -> a = F/m. 
+ * v += a. 
+ * p = v*t
+ */
+Cirkel.prototype.stap = function(t){
 
-	var som = new Vector(0, 0);
+  // Maak een nulvector
+	var som = new Vector();
 	
-	som.add(this.krachten);
+  // Tel alle krachten bij elkaar op
+  this.krachten.forEach(function(el){
+    som.plus(el);
+  });
+  
+  // Deel door de massa
+  som.deel(this.massa);
 	
-	som.dx /= this.massa;
-	som.dy /= this.massa;
+  // Versnelling optellen bij de snelheid
+	this.snelheid.plus(som);
+  
+  // Tel de snelheid op bij de positie.
+  this.positie.plus(this.snelheid.krijgProduct(t));
 	
-	this.snelheid.add(som);
-	
-	this.x += this.snelheid.dx;
-	this.y += this.snelheid.dy;
-	
-	
+  // Voor chaining
 	return this;
 };
